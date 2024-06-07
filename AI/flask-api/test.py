e66255fdecd1736c787bf2d5942c7f975ec6c3a8
Jisu Yeo <duwltn1301@naver.com>
2024-06-07 15:11:46 +0900

    library import 및 image fetch 함수 구현
    pythonanywhere 내부 이미지 사용시 네트워크 에러 발생. 외부 이미지 url 사용할 것.

--- AI/flask-api/test.py
+++ AI/flask-api/test.py
@@ -1,5 +1,32 @@
 from flask_restful import reqparse, Resource
 import requests
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from segmentation_models_pytorch import Unet
+import joblib
+import random
+import string
+torch.backends.cudnn.benchmark = True
+
+def fetch_image(url):
+    try:
+        # 세션을 사용하여 요청
+        with requests.Session() as session:
+            response = session.get(url, timeout=10)  # 타임아웃 설정
+            response.raise_for_status()  # 요청 성공 여부 확인
+            image_data = response.content
+
+        # 바이트 스트림으로부터 이미지 디코딩
+        image_array = np.frombuffer(image_data, np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        return image
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching image from {url}: {e}")
+        return None
 
 class Test(Resource):
     def post(self):
