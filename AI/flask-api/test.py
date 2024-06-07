381ffecd6a7a5affbbd6c166849eec3eb03cbdcc
Jisu Yeo <duwltn1301@naver.com>
2024-06-07 15:12:44 +0900

    classification model과 segmentation 모델 load

--- AI/flask-api/test.py
+++ AI/flask-api/test.py
@@ -29,6 +29,42 @@ def fetch_image(url):
         return None
 
 class Test(Resource):
+    def __init__(self):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f'Using device: {self.device}')
+
+        # classification 모델 초기화
+        self.cls_model = models.efficientnet_b3(pretrained=True)
+        num_ftrs = self.cls_model.classifier[1].in_features
+        self.cls_model.classifier[1] = nn.Linear(num_ftrs, 7)  # 7개의 클래스 출력
+        self.cls_model = self.cls_model.to(self.device)
+        self.cls_model.load_state_dict(torch.load('/home/yeojisu/mysite/file/skin-disease/EfficientNetB3_cls_V1_more_epoch.pth', map_location=torch.device('cpu')))
+        self.cls_model.eval()  # 평가 모드 설정
+        self.cls_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.CenterCrop(300),
+            transforms.Resize(128),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ])
+
+        # Label Encoder 로드
+        self.encoder_path = '/home/yeojisu/mysite/file/skin-disease/label_encoder.pkl'
+        self.le = joblib.load(self.encoder_path)
+
+        # Segmentation 모델 초기화
+        self.seg_model = Unet(encoder_name='resnet34', encoder_weights='imagenet', classes=1, activation='sigmoid')
+        self.seg_model = self.seg_model.to(self.device)
+        self.seg_model.load_state_dict(torch.load('/home/yeojisu/mysite/file/skin-disease/best_model.pth', map_location=self.device))
+        self.seg_model.eval()
+        self.seg_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.CenterCrop(300),
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        ])
+
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("image_url")
